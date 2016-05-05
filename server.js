13767c36180f51f5da0ed6df359d7ecdc3799134
smvargas787 <smvargas@fullsail.edu>
2016-05-04 21:50:20 -0400

    Fixed minor typo

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, "./SpousesInArms")))
+app.use(express.static(path.resolve(__dirname, "./static")))
 
 app.listen(3000);
